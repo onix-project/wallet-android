b0b147b7cd2c0a5ca406dc39cd4a6abb3a19c640
Jose Luis Estevez <jose.estevez.prieto@gmail.com>
2018-11-01 10:45:24 +0100

    Upgrade stratumj electrum client

--- core/src/main/java/com/onix/stratumj/StratumClient.java
+++ core/src/main/java/com/onix/stratumj/StratumClient.java
@@ -141,7 +141,7 @@ public class StratumClient extends AbstractExecutionThreadService {
         }
         try {
             socket = createSocket();
-            log.debug("Creating I/O streams to socket: {}", socket);
+            log.info("Creating I/O streams to socket: {}", socket);
             toServer = new DataOutputStream(socket.getOutputStream());
             fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         } catch (Exception e) {
@@ -255,7 +255,7 @@ public class StratumClient extends AbstractExecutionThreadService {
         try {
             toServer.writeBytes(message.toString());
             callers.put(message.getId(), future);
-        } catch (Exception e) {
+        } catch (Throwable e) {
             future.setException(e);
             log.error("Error making a call to the server: {}", e.getMessage());
             triggerShutdown();
